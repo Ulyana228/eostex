ecb937c998ec314244f921e5a8dbb4f5651eeaf4
Ulyana228 <90844953+Ulyana228@users.noreply.github.com>
2023-06-08 17:54:27 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,81 @@
+let tg = window.Telegram.WedApp;
+
+tg.expand();
+
+tg.MainButton.textColor = "FFFFFF";
+tg.MainButton.color = "#2cab37";
+
+let item = "";
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
+
+btn1.addEventListener("click", function(){
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вы выбрали услугу СЭД «ДЕЛО»!");
+        item ="1";
+        tg.MainButton.show();
+    }
+});
+
+btn2.addEventListener("click", function(){
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вы выбрали услугу СЭД «Архивное дело»!");
+        item ="2";
+        tg.MainButton.show();
+    }
+});
+
+btn3.addEventListener("click", function(){
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вы выбрали услугу «EOS for SharePoint»!");
+        item ="3";
+        tg.MainButton.show();
+    }
+});
+
+btn4.addEventListener("click", function(){
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вы выбрали услугу «Мобильное приложение»!");
+        item ="4";
+        tg.MainButton.show();
+    }
+});
+
+btn5.addEventListener("click", function(){
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вы выбрали услугу «Система криптографического обеспечения КАРМА!»");
+        item ="5";
+        tg.MainButton.show();
+    }
+});
+
+Telegram.WedApp.onEvent("mainButtonClicked", function(){
+    tg.sendData(item);
+});
+
+let usercard = document.getElementById("usercard");
+
+let p = document.createElement("p");
+
+p.innerText = `${tg.initDataUnsafe.first_name} 
+${tg.initDataUnsafe.last_name}`; 
+
+usercard.appendChild(p);    
